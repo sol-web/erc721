c116b12e6a38bac1c48d0390c3ad3c5f3fc2ad82
sol-web <sesi3home@gmail.com>
2023-11-22 10:07:19 +0900

    first commit

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,7 +2,7 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const contractConst = "0xB2e0B7b5A9Ec4D51DeB3D9EF1FD59Ed674D0D7a1";
 
 // The name of the chain your contract is deployed to.
 // Refer to README.md on how to specify the chain name.
@@ -12,7 +12,8 @@ export const chainConst = "mumbai";
 // Please ensure that you define the correct domain for your API key from the API settings page.
 // You can get one for free at https://thirdweb.com/create-api-key
 // Learn more here: https://blog.thirdweb.com/changelog/api-keys-to-access-thirdweb-infra
-export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "";
+export const clientIdConst =
+  import.meta.env.VITE_TEMPLATE_CLIENT_ID || "0dc7c32a9f530adf3e858d3b63486f2d";
 
 // Configure the primary color for buttons and other UI elements
 export const primaryColorConst = "blue";
